07aea196279c584c7246fbf21b06ed7d9ad75be8
KP-1306 <kbisht786@gmail.com>
2025-10-16 10:08:58 +0530

    Delete web/src/components/rbac.ts

--- web/src/components/rbac.ts
+++
@@ -1,3 +0,0 @@
-export type Role = "owner" | "manager" | "staff" | "guest";
-export const hasRole = (userRole?: Role, allowed: Role[] = []) =>
-  allowed.length === 0 ? true : !!userRole && allowed.includes(userRole);

a58965010ab5196a943dd105babd5a7d68df0cc8
KP-1306 <kbisht786@gmail.com>
2025-10-17 16:18:11 +0530

    Create Home.tsx

---
+++ web/src/routes/Home.tsx
@@ -0,0 +1,11 @@
+// web/src/routes/Home.tsx
+import HeroCarousel from "../components/HeroCarousel";
+
+export default function Home() {
+  return (
+    <main>
+      <HeroCarousel />
+      {/* …your landing sections… */}
+    </main>
+  );
+}
